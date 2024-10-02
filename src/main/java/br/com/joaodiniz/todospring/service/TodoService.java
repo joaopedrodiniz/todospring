192bd8c99aaf170701911e03fb299a0ca0338d7a
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2024-10-01 21:20:14 -0300

    feat: Criando service de listagem

---
+++ src/main/java/br/com/joaodiniz/todospring/service/TodoService.java
@@ -0,0 +1,42 @@
+package br.com.joaodiniz.todospring.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.joaodiniz.todospring.entity.Todo;
+import br.com.joaodiniz.todospring.repository.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by("prioridade").descending().and(
+            Sort.by("nome").ascending());
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> delete() {
+
+    }
+}
