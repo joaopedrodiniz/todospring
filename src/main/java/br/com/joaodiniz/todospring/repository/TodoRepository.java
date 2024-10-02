7ec4ee39c8dcc47e29020236eabd093937097278
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2024-10-01 21:03:55 -0300

    feat: Criando o repository

---
+++ src/main/java/br/com/joaodiniz/todospring/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package br.com.joaodiniz.todospring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.joaodiniz.todospring.entity.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    
+    
+} 
