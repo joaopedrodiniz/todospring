eacc546526dc939d70ed76457a9ffb885850ef47
João Pedro Diniz <joaopedrodiniz44@gmail.com>
2024-10-01 22:06:26 -0300

    feat: adicionando application properties

--- src/main/java/br/com/joaodiniz/todospring/repository/TodoRepository.java
+++ src/main/java/br/com/joaodiniz/todospring/repository/TodoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.joaodiniz.todospring.entity.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    
+
     
 } 
